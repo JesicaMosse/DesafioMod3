c67ad28ee2156633eaabaaa88b111836ba0ede1f
Jesica <jesicamosse@gmail.com>
2022-09-20 12:14:26 -0300

    Agrego Redes

--- Portfolio_dinamico/src/app/app.module.ts
+++ Portfolio_dinamico/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
 import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
+import { RedsocialComponent } from './components/redsocial/redsocial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedsocialComponent
   ],
   imports: [
     BrowserModule,
