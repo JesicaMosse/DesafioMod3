7f92d2075a527ccdfe014592014194de0d58e4f1
Jesica <jesicamosse@gmail.com>
2022-09-20 21:00:49 -0300

    agrego bootstrap

--- Portfolio_dinamico/src/app/app.module.ts
+++ Portfolio_dinamico/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
 import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
 import { RedsocialComponent } from './components/redsocial/redsocial.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedsocialComponent
+    RedsocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
